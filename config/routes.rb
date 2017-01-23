b7f0c65d385a7bab6a34ca90ca9be68760784492
Jesse L Lawson <homestarflyer@gmail.com>
2017-01-22 19:23:47 -0500

    added navbar in shared file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
 
+  root 'pages#home'
+  devise_for :users,
+             path: '',
+             path_names: {sign_in:'login', sign_out: 'logout', edit: 'profile'},
+             controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+  resources :users, only: [:show]
+  
   get '/privacy_policy' => 'pages#privacy_policy'
 end
