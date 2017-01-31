afc10186783ca14be08d88f142436b9891d272f7
Jesse L Lawson <homestarflyer@gmail.com>
2017-01-30 19:18:40 -0500

    create index and new form for bikes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,19 +1,25 @@
 class BikesController < ApplicationController
   before_action :set_bike, only: [:show, :edit, :update, :destroy]
 
+  #TODO must add photos to bikes
 
   def index
-    @bikes = Bike.all # soon to use search params ###### .search(params[:field], params[:search])
+    @bikes = Bike.all #TODO soon to use search params ###### .search(params[:field], params[:search])
   end
 
   def show
   end
 
   def new
-    @bike = Bike.new
+    @bike = current_user.bikes.build
   end
 
   def edit
+    if current_user.id == @bike.user.id ##TODO create admin role || current_user.is_admin?
+      #set @photos
+    else
+      redirect_to root_path, notice: "You don not have permission."
+    end
   end
 
   def create
@@ -33,7 +39,7 @@ class BikesController < ApplicationController
   def update
     respond_to do |format|
       if @bike.update(bike_params)
-        format.html { redirect_to @bike, notice: 'Bike was successfully updated.' }
+        format.html { redirect_to @bike, notice: 'Updated...' }
         format.json { render :show, status: :ok, location: @bike }
       else
         format.html { render :edit }
