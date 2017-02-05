462b94495360dfdfc3dbb77a95de8467ca9230b7
Jesse L Lawson <homestarflyer@gmail.com>
2017-02-04 21:20:12 -0500

    add users bike panels to profile page

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -16,6 +16,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [300,300]
   end
 
+  version :thumb do
+    process :resize_to_fill => [100,100]
+  end
+
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
