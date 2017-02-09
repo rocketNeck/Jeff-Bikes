a06f9f1182eb08ffe7e0a076b001bbe2c5a6887f
Jesse L Lawson <homestarflyer@gmail.com>
2017-02-08 20:15:43 -0500

    generate conversation and message model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def home
+    if user_signed_in?
+      redirect_to :controller=>'bikes', :action => 'index'
+    end
   end
 
   def privacy_policy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205233110) do
+ActiveRecord::Schema.define(version: 20170209011319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,23 @@ ActiveRecord::Schema.define(version: 20170205233110) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -87,4 +104,6 @@ ActiveRecord::Schema.define(version: 20170205233110) do
   add_foreign_key "bike_tags", "bikes"
   add_foreign_key "bike_tags", "tags"
   add_foreign_key "bikes", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
