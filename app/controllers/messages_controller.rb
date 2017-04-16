46296188b42c907a8decd54f1cecf89199048c3c
Jesse L Lawson <homestarflyer@gmail.com>
2017-04-16 18:32:38 -0400

    set message#create to render json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
     @messages = @conversation.messages.order("created_at DESC")
 
     if @message.save
-      render 'create.js', :layout => false
+      render json: @message
     end
   end
 
