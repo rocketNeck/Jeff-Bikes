9ecda1dbb694c25b72a88c0639594169aa1e6f21
Jesse L Lawson <homestarflyer@gmail.com>
2017-04-26 18:32:28 -0400

    format users/show response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @bikes = @user.bikes
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @user}
+    end
   end
 end
